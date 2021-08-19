b17736b15829db29bc443134cfff2a46e66ea8e7
Renato Moreno <renats.mc@gmail.com>
2021-08-19 20:02:40 -0300

    estilizando o forms com bootstrap

--- modulo1-fundamentos/bloco6/6.2-bibliotecas-js-e-frameworks-css/script.js
+++ modulo1-fundamentos/bloco6/6.2-bibliotecas-js-e-frameworks-css/script.js
@@ -1,4 +1,4 @@
-const arrayEstados = ['Selecione seu estado','Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato rosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+const arrayEstados = ['Selecione seu estado','Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
 
 for (let index = 0; index < arrayEstados.length; index += 1) {
   let estado = arrayEstados[index];
