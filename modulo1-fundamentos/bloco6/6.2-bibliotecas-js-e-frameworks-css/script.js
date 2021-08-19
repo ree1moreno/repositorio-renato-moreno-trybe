bda4cd983a4a0581ec3c27a71f2d3161f494bb10
Renato Moreno <renats.mc@gmail.com>
2021-08-19 16:53:50 -0300

    iniciando exercicios bloco6.2

---
+++ modulo1-fundamentos/bloco6/6.2-bibliotecas-js-e-frameworks-css/script.js
@@ -0,0 +1,13 @@
+const arrayEstados = ['Selecione seu estado','Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato rosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+
+for (let index = 0; index < arrayEstados.length; index += 1) {
+  let estado = arrayEstados[index];
+  let opcao = document.createElement('option');
+  opcao.value = estado;
+  opcao.innerText = estado;
+  let selecao = document.querySelector('#estados');
+  selecao.appendChild(opcao);
+}
+
+const botaoEnviar = document.querySelector('#botao');
+
