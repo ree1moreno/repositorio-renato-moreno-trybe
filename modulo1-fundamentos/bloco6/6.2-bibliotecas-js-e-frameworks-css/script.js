812d90375eb7baec1d0c1ce210b02a9b3b4dc42d
Renato Moreno <renats.mc@gmail.com>
2021-08-24 13:06:34 -0300

    tentando usaro justvalidate - sem sucesso

--- modulo1-fundamentos/bloco6/6.2-bibliotecas-js-e-frameworks-css/script.js
+++ modulo1-fundamentos/bloco6/6.2-bibliotecas-js-e-frameworks-css/script.js
@@ -1,13 +1,21 @@
-const arrayEstados = ['Selecione seu estado','Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
 
-for (let index = 0; index < arrayEstados.length; index += 1) {
-  let estado = arrayEstados[index];
-  let opcao = document.createElement('option');
-  opcao.value = estado;
-  opcao.innerText = estado;
-  let selecao = document.querySelector('#estados');
-  selecao.appendChild(opcao);
+function criarEstados() {
+  const arrayEstados = ['Selecione seu estado','Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+  
+  for (let index = 0; index < arrayEstados.length; index += 1) {
+    let estado = arrayEstados[index];
+    let opcao = document.createElement('option');
+    opcao.value = estado;
+    opcao.innerText = estado;
+    let selecao = document.querySelector('#estados');
+    selecao.appendChild(opcao);
+  }
+  
 }
 
-const botaoEnviar = document.querySelector('#botao');
+window.onload = function () {
+criarEstados();
+
+};
+
 
