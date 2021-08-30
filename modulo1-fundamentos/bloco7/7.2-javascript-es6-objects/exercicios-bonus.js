4d76dafc405a23100e71c7d912f1a2a9ed758e86
Renato Moreno <renats.mc@gmail.com>
2021-08-30 16:22:39 -0300

    adiciona exercicios ainda nao resolvidos

---
+++ modulo1-fundamentos/bloco7/7.2-javascript-es6-objects/exercicios-bonus.js
@@ -0,0 +1,2 @@
+// 1. Crie uma função para contar quantos estudantes assistiram às aulas de Matemática. Use o objeto criado no exercício 5.
+// 2. Crie uma função que deverá retornar um objeto que representa o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes. Use o objeto criado no exercício 5:
