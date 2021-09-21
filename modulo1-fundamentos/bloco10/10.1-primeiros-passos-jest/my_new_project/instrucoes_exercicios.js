55d76937640e989754a4887f975960e1d84ebee3
Renato Moreno <renats.mc@gmail.com>
2021-09-21 19:07:49 -0300

    exercicios parte 1

---
+++ modulo1-fundamentos/bloco10/10.1-primeiros-passos-jest/my_new_project/instrucoes_exercicios.js
@@ -0,0 +1,15 @@
+// Parte I
+// Refaça os exercícios 1 a 5 do conteúdo de Testes unitários em JavaScript , dessa vez utilizando Jest .
+
+
+// Parte II
+// No Bloco 4 você desenvolveu um projeto para testar os seus conhecimentos em Javascript e lógica de programação: o Playground functions . Nos exercícios a seguir, você irá trabalhar com os testes para algumas funções que você criou! Aproveite para refatorá-las e usar todos os recursos que já aprendemos até aqui, como as Higher Order Functions e as features do Javascript ES6.
+// Para as funções encode e decode crie os seguintes testes:
+// Teste se encode e decode são funções;
+// Para a função encode teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente;
+// Para a função decode teste se os números 1, 2, 3, 4 e 5 são convertido nas vogais a, e, i, o, u , respectivamente;
+// Teste se as demais letras/números não são convertidos para cada caso;
+// Teste se a string que é retornada pelas funções têm o mesmo número de caracteres que a string passada como parâmetro.
+
+
+// A função techList recebe como parâmetros um array contendo uma lista de tecnologias e uma string com um nome. Para cada tecnologia no array a função cria, em ordem alfabética, um objeto com a seguinte estrutura:
--- modulo1-fundamentos/bloco10/10.1-primeiros-passos-jest/my_new_project/sum.test.js
+++ modulo1-fundamentos/bloco10/10.1-primeiros-passos-jest/my_new_project/sum.test.js
@@ -1,5 +1,18 @@
-const sum = (a, b) => a + b;
+// sum.test.js
+const sum = require('./sum');
 
-test('sums two values', () => {
-  expect(sum(2, 3)).toEqual(5);
+test('4 mais 5 é igual a 9', () => {
+  expect(sum(4, 5)).toBe(9);
+});
+
+test('0 mais 0 é igual a 0', () => {
+  expect(sum(0, 0)).toBe(0);
+});
+
+test('mostrar um erro caso um parametro seja do tipo string', () => {
+  expect(() => sum(4, '5')).toThrow();
+});
+
+test('mensagem de erro dizendo que os parametros devem ser números', () => {
+  expect(() => sum(4, '5')).toThrow();
 });
